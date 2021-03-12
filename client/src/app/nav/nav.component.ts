9e90ae9253eaec096e723e3ba9db7bbbe27d6b01
avadhvashisth <avadh.vashisth@gmail.com>
2021-03-12 17:18:15 +0530

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -19,9 +19,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe((response: any) => {
       this.router.navigateByUrl('/members');
       this.model = {};
-    }, (error: any) => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
