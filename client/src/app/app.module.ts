09b88b14970fbfb68828119b3b0292338c19ad53
avadhvashisth <avadh.vashisth@gmail.com>
2021-03-19 19:29:35 +0530

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LodingInterceptor } from './_interceptors/loding.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { LodingInterceptor } from './_interceptors/loding.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
