eee595beb2f9975e535cd900e1b07f8a59014c11
avadhvashisth <avadh.vashisth@gmail.com>
2021-03-30 15:58:01 +0530

    End of section 13

--- API/Extentions/ClaimsPrincipleExtensions.cs
+++ API/Extentions/ClaimsPrincipleExtensions.cs
@@ -4,8 +4,14 @@ namespace API.Extentions
 {
     public static class ClaimsPrincipleExtensions
     {
-        public static string GetUsername(this ClaimsPrincipal user){
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
         }
     }
 }
